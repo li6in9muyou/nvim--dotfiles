25135af71dca3d6f83402212ef6591f363209f70
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-03-24 19:57:27 +0800

    ok: config numbers

--- init.lua
+++ init.lua
@@ -102,7 +102,10 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
+
+-- Set my shell to pwsh 7
+vim.opt.shell = '"C:\\Program Files\\PowerShell\\7-preview\\pwsh.exe" -nologo'
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
