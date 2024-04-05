5e690fa4df734fb2260082f54790c36d8a338ea0
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-05 10:49:49 +0800

    chore: add more language support

--- init.lua
+++ init.lua
@@ -620,7 +620,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -902,7 +902,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'python', 'javascript', 'typescript', 'go' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
