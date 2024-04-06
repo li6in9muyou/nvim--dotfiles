366afe9c564a27bd0d8a3d321d32e1f9eb8cc1da
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-06 14:59:07 +0800

    chore: disable searching nvim config

--- init.lua
+++ init.lua
@@ -481,9 +481,9 @@ require('lazy').setup({
       end, { desc = '[S]earch [/] in Open Files' })
 
       -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+      -- vim.keymap.set('n', '<leader>sn', function()
+      --   builtin.find_files { cwd = vim.fn.stdpath 'config' }
+      -- end, { desc = '[S]earch [N]eovim files' })
     end,
   },
 
