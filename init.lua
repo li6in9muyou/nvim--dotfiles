6735326a2321cff0abfba1b709dd2ef19dc65590
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-08 22:10:15 +0800

    Revert "ok: save all changed buffers every time I save"
    This reverts commit ed3b648e15737c3dc0b6757033969fe9e605d1d6.

--- init.lua
+++ init.lua
@@ -176,8 +176,6 @@ vim.opt.scrolloff = 10
 --  See `:help vim.keymap.set()`
 vim.keymap.set('n', 'x', '"9x', { desc = "don't mess up my yank" })
 
-vim.keymap.set('n', ':w', ':wa', { desc = 'save all changed buffers every time I save' })
-
 vim.keymap.set('n', '<leader>y', ':%y<CR>', { desc = '[y]ank all in buffer', noremap = true, silent = true })
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
