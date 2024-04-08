547c19a8e9ddb1ffb99ed4a3c10df377c4530bed
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-08 22:09:02 +0800

    ok: document Mason tools in init.lua

--- init.lua
+++ init.lua
@@ -697,6 +697,10 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
+        black = {},
+        pylint = {},
+        docformatter = {},
+        pylsp = {},
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
