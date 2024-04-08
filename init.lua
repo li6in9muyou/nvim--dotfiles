c75c29dacb2099c9933f8ad177989f4f1717b378
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-08 22:07:44 +0800

    refactor: set ensure_installed in one place

--- init.lua
+++ init.lua
@@ -706,6 +706,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
+        stylua = {},
         --
 
         lua_ls = {
@@ -735,9 +736,6 @@ require('lazy').setup({
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
