790439d9b16a10c43176545d9b424a78fb55ccfd
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-09 17:25:04 +0800

    ok: kj or jk to exit insert mode

--- init.lua
+++ init.lua
@@ -1115,6 +1115,8 @@ vim.filetype.add {
 }
 
 vim.keymap.set('n', '<leader>e', require('lsp_lines').toggle, { desc = 'Toggle diagnostic [e]rror messages' })
+vim.keymap.set('i', 'jk', '<Esc>', { silent = true })
+vim.keymap.set('i', 'kj', '<Esc>', { silent = true })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
