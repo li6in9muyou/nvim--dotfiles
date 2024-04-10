e1885d13fb57cd0fe79c457dda8c894fc7ac9aeb
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-10 18:53:37 +0800

    ok: double tap k to exit insert mode

--- init.lua
+++ init.lua
@@ -1122,8 +1122,7 @@ vim.filetype.add {
 }
 
 vim.keymap.set('n', '<leader>e', require('lsp_lines').toggle, { desc = 'Toggle diagnostic [e]rror messages' })
-vim.keymap.set('i', 'jk', '<Esc>', { silent = true })
-vim.keymap.set('i', 'kj', '<Esc>', { silent = true })
+vim.keymap.set('i', 'kkk', '<Esc>:w<CR>', { silent = true })
 vim.keymap.set('n', '<leader>vt', ':%s/\\s\\+$//e', { desc = 'remove [t]railing whitespaces', noremap = true })
 vim.keymap.set('n', '<leader>va', 'm0gg<S-v><S-g>', { desc = "select [a]ll in buffer, use '0 to go back", noremap = true, silent = true })
 -- The line beneath this is called `modeline`. See `:help modeline`
