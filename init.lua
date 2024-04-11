ced5270e4d9ad46ad31ad331106ab1c5b4e57530
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-11 22:19:07 +0800

    fix: install ripgrip

--- init.lua
+++ init.lua
@@ -538,6 +538,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      -- The following command requires ripgrip executable in path
+      -- see https://github.com/BurntSushi/ripgrep
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
