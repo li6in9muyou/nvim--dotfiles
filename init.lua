31e9296be7eb4ed09b2b62ac765199d472643b04
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-13 17:41:46 +0800

    Revert "ok: tweak diagnostic messages"
    This reverts commit fb1e360d65ebdadad5442f14ec1c63716c599fcd.

--- init.lua
+++ init.lua
@@ -184,8 +184,12 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
+vim.diagnostic.config { source = true, virtual_text = { spacing = 1 } }
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', function()
+  vim.diagnostic.open_float()
+end, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
@@ -937,17 +941,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-    config = function()
-      require('lsp_lines').setup()
-      vim.diagnostic.config {
-        source = true,
-        virtual_text = false,
-      }
-    end,
-  },
-
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -1132,7 +1125,6 @@ vim.filetype.add {
   },
 }
 
-vim.keymap.set('n', '<leader>e', require('lsp_lines').toggle, { desc = 'Toggle diagnostic [e]rror messages' })
 vim.keymap.set('i', 'kkk', '<Esc>:w<CR>', { silent = true })
 vim.keymap.set('n', '<leader>vt', ':%s/\\s\\+$//e', { desc = 'remove [t]railing whitespaces', noremap = true })
 vim.keymap.set('n', '<leader>va', 'm0gg<S-v><S-g>', { desc = "select [a]ll in buffer, use '0 to go back", noremap = true, silent = true })
