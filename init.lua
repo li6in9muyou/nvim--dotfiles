56623bc39837c9d58b5da4ab62cb40f886d082ed
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-13 19:05:28 +0800

    ok: tweak keymap; rearrange code;

--- init.lua
+++ init.lua
@@ -542,15 +542,15 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      -- The following command requires ripgrip executable in path
-      -- see https://github.com/BurntSushi/ripgrep
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
+      -- The following command requires ripgrip executable in path
+      -- see https://github.com/BurntSushi/ripgrep
+      vim.keymap.set('n', '<leader>?', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
