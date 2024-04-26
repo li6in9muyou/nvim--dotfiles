4542ea9dd66842d6171ff4dec96e24f44691f65d
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-26 15:03:20 +0800

    ok: :Here to open file directory

--- init.lua
+++ init.lua
@@ -1278,5 +1278,8 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter' }, {
   command = ':checktime',
 })
 
+-- :here to open buffer file parent
+vim.api.nvim_create_user_command('Here', ':e %:h', { desc = 'open buffer file parent' })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
