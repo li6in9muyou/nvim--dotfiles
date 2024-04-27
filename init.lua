0be948cd5b46d8c3622b63a4e20e2238ef3994da
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-04-27 16:34:55 +0800

    fix: oil option is overwritten with oil.setup(); keep oil buffer in buffer list;

--- init.lua
+++ init.lua
@@ -1098,31 +1098,37 @@ require('lazy').setup({
 
   {
     'stevearc/oil.nvim',
-    opts = {
-      keymaps = {
-        ['g?'] = 'actions.show_help',
-        ['<CR>'] = 'actions.select',
-        ['<C-s>'] = 'actions.select_vsplit',
-        -- ['<C-h>'] = 'actions.select_split',
-        -- ['<C-t>'] = 'actions.select_tab',
-        ['<C-p>'] = 'actions.preview',
-        ['<C-c>'] = 'actions.close',
-        -- ['<C-l>'] = 'actions.refresh',
-        ['-'] = 'actions.parent',
-        ['_'] = 'actions.open_cwd',
-        ['`'] = 'actions.cd',
-        ['~'] = 'actions.tcd',
-        ['gs'] = 'actions.change_sort',
-        ['gx'] = 'actions.open_external',
-        ['g.'] = 'actions.toggle_hidden',
-        ['g\\'] = 'actions.toggle_trash',
-      },
-      cleanup_delay_ms = false,
-    },
     -- Optional dependencies
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require('oil').setup()
+      require('oil').setup {
+        skip_confirm_for_simple_edits = true,
+        -- always keep an oil in buffer list
+        cleanup_delay_ms = false,
+        -- always keep an oil in buffer list
+        buf_options = {
+          buflisted = true,
+          bufhidden = 'hide',
+        },
+        keymaps = {
+          ['g?'] = 'actions.show_help',
+          ['<CR>'] = 'actions.select',
+          ['<C-s>'] = 'actions.select_vsplit',
+          -- ['<C-h>'] = 'actions.select_split',
+          -- ['<C-t>'] = 'actions.select_tab',
+          ['<C-p>'] = 'actions.preview',
+          ['<C-c>'] = 'actions.close',
+          -- ['<C-l>'] = 'actions.refresh',
+          ['-'] = 'actions.parent',
+          ['_'] = 'actions.open_cwd',
+          ['`'] = 'actions.cd',
+          ['~'] = 'actions.tcd',
+          ['gs'] = 'actions.change_sort',
+          ['gx'] = 'actions.open_external',
+          ['g.'] = 'actions.toggle_hidden',
+          ['g\\'] = 'actions.toggle_trash',
+        },
+      }
     end,
   },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
