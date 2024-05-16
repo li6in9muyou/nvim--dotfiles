4536d67d5a4d885a4fb1b79e189cad5200853253
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-05-16 09:07:15 +0800

    chore: async formatting still throws error
    This reverts commit e751da9b4e4d0562e357f6a5fed6edc0af3ef8b1.

--- init.lua
+++ init.lua
@@ -918,7 +918,7 @@ require('lazy').setup({
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         return {
-          async = true,
+          timeout_ms = 1500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
