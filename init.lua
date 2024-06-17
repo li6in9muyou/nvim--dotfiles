810217b3a86b5b78577c793d3d42c1d20d0b5eb7
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-06-17 20:58:42 +0800

    fix: weired signs in sign-column

--- init.lua
+++ init.lua
@@ -233,6 +233,7 @@ require('lazy').setup({
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
+      signs_staged_enable = false,
       signs = {
         add = { text = '🟩' },
         change = { text = '✒️' },
