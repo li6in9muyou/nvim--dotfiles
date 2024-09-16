e5825404d2e5da7752c41719969389b153592130
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-09-16 15:22:58 +0800

    ok: add some colors

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'NvChad/nvim-colorizer.lua',
+    config = function()
+      vim.opt.termguicolors = true
+      require('colorizer').setup { filetypes = { '*' }, user_default_options = { css = true } }
+    end,
+  },
+}
