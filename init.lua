fb4cc0e9aa0be47e4eade7d060137d56a04d0804
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-05-30 23:20:25 +0800

    ok: tweak surround config because empty spaces are stupid

--- init.lua
+++ init.lua
@@ -1105,7 +1105,18 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        custom_surroundings = {
+          ['('] = { output = { left = '(', right = ')' } },
+          [')'] = { output = { left = '( ', right = ' )' } },
+
+          ['['] = { output = { left = '[', right = ']' } },
+          [']'] = { output = { left = '[ ', right = ' ]' } },
+
+          ['{'] = { output = { left = '{', right = '}' } },
+          ['}'] = { output = { left = '{ ', right = ' }' } },
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
