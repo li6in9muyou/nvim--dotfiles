5c20ec2310695733639f6333909ccf376b92e947
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-07-15 20:20:52 +0800

    ok: auto complete words from all visible buffers

--- init.lua
+++ init.lua
@@ -1129,7 +1129,18 @@ require('lazy').setup({
         },
         sources = {
           { name = 'nvim_lsp' },
-          { name = 'buffer' },
+          {
+            name = 'buffer',
+            option = {
+              get_bufnrs = function()
+                local visible_bufs = {}
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                  visible_bufs[vim.api.nvim_win_get_buf(win)] = true
+                end
+                return vim.tbl_keys(visible_bufs)
+              end,
+            },
+          },
           {
             name = 'buffer-lines',
             option = {
